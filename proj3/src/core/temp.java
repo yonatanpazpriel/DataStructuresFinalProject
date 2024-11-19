85bbfaf1c9775e804d59868591c69c958e5687ef
Yonatan <yonatanpazpriel@berkeley.edu>
2024-11-19 13:02:10 -0800

    catching up with final 3a submission

--- proj3/src/core/temp.java
+++ proj3/src/core/temp.java
@@ -1,19 +1,31 @@
-/*
-int currQuad = -1;
-        HashMap<Integer, Integer> quadToAmountFilled = new HashMap<>();
+private void eraseDeadEnds() {
+    boolean anyDeadEnds = true;
+    while (anyDeadEnds) {
+        anyDeadEnds = false;
         for (int i = 0; i < WIDTH; i++) {
             for (int j = 0; j < HEIGHT; j++) {
-                boolean marked = world[j][i] != 0;
-                if (i < WIDTH / 2 && j < HEIGHT / 2 && marked) {
-                    topLeftQuad++;
-                } else if (i > WIDTH / 2 && j < HEIGHT / 2 && marked) {
-                    topRightQuad++;
-                } else if (i < WIDTH / 2 && j > HEIGHT / 2 && marked) {
-                    bottomLeftQuad++;
-                } else if (i > WIDTH / 2 && j > HEIGHT / 2 && marked) {
-                    bottomRightQuad++;
+                if (world[j][i] != 0 && deadEndChecker(i, j)) {
+                    world[j][i] == 0;
+                    anyDeadEnds = true;
                 }
             }
         }
-        quadToAmountFilled.put()
- */
+    }
+}
+
+private boolean deadEndChecker(int i, int j) {
+    int neighbors = 0;
+    if (j + 1 <= HEIGHT && world[j + 1][i] != 0) { // right
+        neighbors++;
+    }
+    if (i - 1 >= 0 && world[j][i - 1] != 0) { //  bottom
+        neighbors++;
+    }
+    if (j - 1 >= 0 && world[j - 1][i] != 0) { // left
+        neighbors++;
+    }
+    if (i + 1 < WIDTH && world[j][i + 1] != 0) { // top
+        neighbors++;
+    }
+    return neighbors < 2;
+}
