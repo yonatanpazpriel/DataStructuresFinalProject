2711df3cb2ff209c7b295ed5e53b710c1c5734d8
Yonatan <yonatanpazpriel@berkeley.edu>
2024-11-25 18:12:21 -0800

    done working. only issue might be blobs

--- proj3/src/core/InteractiveWorld2.java
+++ proj3/src/core/InteractiveWorld2.java
@@ -4,7 +4,6 @@ import edu.princeton.cs.algs4.StdDraw;
 import tileengine.TERenderer;
 import tileengine.TETile;
 import tileengine.Tileset;
-import java.awt .*;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
@@ -65,6 +64,7 @@ public class InteractiveWorld2 {
             }
         }
     }
+
     private void generateRandomCharacter(int x, int y) {
         for (int i = x; i <= WIDTH - 2; i++) {
             for (int j = y; j <= HEIGHT - 2; j++) {
@@ -88,13 +88,7 @@ public class InteractiveWorld2 {
     }
 
     private TETile[][] darkness(TETile[][] world, int x, int y) {
-        /*
-        for (int i = 0; i <= WIDTH - 2; i++) {
-            for (int j = 0; j <= HEIGHT - 2; j++) {
-                darkWorld[i][j] = Tileset.NOTHING;
-            }
-        }
-        */
+
         TETile[][] temp = copy(world);
         for (int i = 0; i < WIDTH; i++) {
             for (int j = 0; j < HEIGHT; j++) {
@@ -107,49 +101,6 @@ public class InteractiveWorld2 {
             }
         }
         return temp;
-    }
-
-    public static void fill(TETile[][] world, int x, int y, char c) {
-        TETile tile = new TETile(c, Color.cyan, Color.black, " ", 0);
-        world[x][y] = tile;
-    }
-
-    public static void play(TETile[][] world) {
-        TERenderer ter = new TERenderer();
-        ter.initialize(WIDTH, HEIGHT);
-        char c;
-        while (true) {
-            while (StdDraw.hasNextKeyTyped()) {
-                c = Character.toLowerCase(StdDraw.nextKeyTyped());
-                if (c == "l".toCharArray()[0] || c == "n".toCharArray()[0] || c == "q".toCharArray()[0]) {
-                    //
-                }
-            }
-            ter.renderFrame(world);
-        }
-    }
-
-
-    public static TETile[][] createWorld(long longSeed) {
-        World2 x = new World2(longSeed);
-        int [][] yippee = x.world;//.createWorld();
-        TERenderer ter = new TERenderer();
-        ter.initialize(WIDTH, HEIGHT);
-        TETile[][] world = new TETile[WIDTH][HEIGHT];
-        for (int i = 0; i< WIDTH; i++) {
-            for (int y = 0; y < HEIGHT; y++) {
-                world[y][i] = Tileset.NOTHING;
-                if (yippee[y][i] == 1) {
-                    world[y][i] = Tileset.CELL;
-                } if (yippee[y][i] == 2) {
-                    world[y][i] = Tileset.CELL;
-                }else if (yippee[y][i] == 3) {
-                    world[y][i] = Tileset.WALL;
-                }
-            }
-        }
-        ter.renderFrame(world);
-        return world;
     }
 
     public InteractiveWorld2 startPlaying() {
